9f4ac88d2a3c3b68abe77dfff3f8a2ab8610b9c5
Aleksey Bogush <71962603+Bogushukr@users.noreply.github.com>
2021-12-13 20:52:28 +0200

    add my pet

---
+++ app.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board');
+const colors = ['red', '#e74c3c', 'green', '#e67e22', 'blue', '#2ecc71', 'white', 'purple', 'yellow', 'green', '#72fc01',
+'#0105fc', '#a217ff', '#10fabf', '#fc01bd', '#fcac01', '#1ca8f8', '#2f01fc', '#a0226c', '#4a7e61'];
+const SQUARES_NUMBER = 2040;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () =>
+    setColor(square));
+
+    square.addEventListener('mouseleave', () =>
+    removeColor(square));
+
+    board.append(square);
+};
+
+function setColor(el) {
+    const color = getRandomColor();
+    el.style.backgroundColor = color;
+    el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+};
+
+function removeColor(el) {
+    el.style.backgroundColor = '#1d1d1d';
+    el.style.boxShadow = `0 0 2px #000`
+};
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index]
+};
