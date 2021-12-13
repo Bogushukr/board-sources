4f5a0a2aeb8b8cc1b683531e1eb24c1bb3b63669
Aleksey Bogush <71962603+Bogushukr@users.noreply.github.com>
2021-12-14 00:14:46 +0200

    update app.js

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
 
+    // square.addEventListener('mouseover', setColor);
     square.addEventListener('mouseover', () =>
     setColor(square));
 
+    // square.addEventListener('mouseleave', removeColor);
     square.addEventListener('mouseleave', () =>
     removeColor(square));
 
@@ -17,17 +19,22 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
 };
 
 function setColor(el) {
+    // function setColor(event) { 
+    // const el = event.target;
     const color = getRandomColor();
     el.style.backgroundColor = color;
     el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 };
 
 function removeColor(el) {
+    // function removeColor(event) {
+    // const el = event.target;
     el.style.backgroundColor = '#1d1d1d';
     el.style.boxShadow = `0 0 2px #000`
 };
 
 function getRandomColor() {
-    const index = Math.floor(Math.random() * colors.length);
-    return colors[index]
+    // const index = Math.floor(Math.random() * colors.length);
+    // return colors[index]
+    return colors[Math.floor(Math.random() * colors.length)];
 };
